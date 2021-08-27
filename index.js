27e697973dddd76fc17441d8b0900fa8dca852ee
Tommie <devtommie@gmail.com>
2021-08-27 10:49:26 -0400

    Added comments to better explain the code

--- index.js
+++ index.js
@@ -5,23 +5,30 @@
 */
 
 /* TODO */
-// ADD COMMENTS TO EXPLAIN CODE
+// ADD COMMENTS TO 'interactionCreate' TO BETTER UNDERSTAND IT
+// ADD COMMENTS TO 'deploy-commands.js' TO BETTER UNDERSTAND IT
 
 // REQUIRED PACKAGES
 require('dotenv').config();
 const fileSystem = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 
+// CREATE CLIENT INSTANCE
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// CREATE COLLECTION OF COMMANDS
 client.commands = new Collection();
+
+// SORT THROUGH COMMAND FOLDER TO LOOK FOR .JS FILES
 const commandFiles = fileSystem.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// STORE COMMAND FILES FOUND INTO AN ARRAY
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
+// CONSOLE LOG WHEN CLIENT IS ONLINE
 client.once('ready', () => {
 	console.log(`${client.user.tag} is online!`);
 });
@@ -42,4 +49,5 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+// LOGIN INTO CLIENT INSTANCE USING TOKEN
 client.login(process.env.TOKEN_KEY);
