ba717dff768e99e3d42dbfb8d9e078e8c6b3e3cc
Tommie <devtommie@gmail.com>
2021-09-14 23:46:38 -0400

    Created the userinfo command. Adjusted the description for the avatarviewer command. Added the moment package to easily display dates for userinfo command.

---
+++ src/commands/information/userinfo.js
@@ -0,0 +1,41 @@
+// REQUIRE PACKAGES
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+// EXPORT SERVERINFO COMMAND DATA TO NODE
+module.exports = ({
+	data: new SlashCommandBuilder()
+		.setName('userinfo')
+		.setDescription('Display a users profile information.')
+		.addUserOption(option =>
+			option.setName('target')
+				.setDescription('Target a user...')
+				.setRequired(false)),
+	async execute(interaction) {
+		// REFERENCE THE TARGET
+		let member = interaction.options.getMember('target');
+		// IF NO TARGET ASSIGN THE USER TO THE VARAIBLE
+		if (interaction.options.getUser('target') === null) {
+			member = interaction.member;
+		}
+		// CREATE TEST EMBED
+		const whoisEmbed = new MessageEmbed();
+		whoisEmbed.setColor('#36393F');
+		// whoisEmbed.setTitle(`${user.tag}'s Avatar`);
+		whoisEmbed.setAuthor('Fyce Bot - /userinfo', member.user.avatarURL(), 'https://github.com/ttommie/fyce-bot/');
+		whoisEmbed.setDescription(`${member}`);
+		whoisEmbed.setThumbnail(`${member.user.displayAvatarURL()}`);
+		whoisEmbed.addFields(
+			{ name: 'Register Date', value: `${moment(member.user.createdAt).format('ddd, DD/MMM/YYYY, h:ss A')}`, inline: true },
+			{ name: '\u200b', value: '\u200b', inline: true },
+			{ name: 'Joined Date', value: `${moment(member.JoinedAt).format('ddd, DD/MMM/YYYY, h:ss A')}`, inline: true },
+			{ name: 'Name', value: `${member.user.username}`, inline: true },
+			{ name: '\u200b', value: '\u200b', inline: true },
+			{ name: 'Nickame', value: `${member.nickname !== null ? `${member.nickname}` : 'None'}`, inline: true },
+			{ name: 'Roles', value: `${member.roles.cache.map(roles => `${roles}`).join(' ')}`, inline: false },
+		);
+		whoisEmbed.setFooter(`Client ID: ${member.id}`);
+		await interaction.reply({ embeds: [whoisEmbed] });
+	},
+});
